cc3d96e80ae42b09b199a3d1f99d44cf4360ef12
rajnath <patelrajnath@gmail.com>
2018-05-27 00:14:30 +0530

    Updated saving/loading checkpoints.

--- nmtutils/utils_training.py
+++ nmtutils/utils_training.py
@@ -137,7 +137,7 @@ def load_model_state(filename, model, cuda_device=None):
             map_location=lambda s, l: default_restore_location(s, 'cuda:{}'.format(cuda_device))
         )
     # state = _upgrade_state_dict(state)
-    state['model'] = model.upgrade_state_dict(state['model'])
+    # state['model'] = model.upgrade_state_dict(state['model'])
 
     # load model parameters
     try:

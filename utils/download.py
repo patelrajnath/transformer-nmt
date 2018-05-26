3a14cdba4a95e9f66fc133cfb490c9ef3f1b30ce
rajnath <patelrajnath@gmail.com>
2018-05-26 12:34:48 +0530

    Refatored the package.

---
+++ utils/download.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+"""
+ Copyright (c) 2018 Raj Nath Patel
+ Licensed under the GNU Public License.
+ Author: Raj Nath Patel
+ Email: patelrajnath (at) gmail (dot) com
+ Created: 25/May/2018 12:27
+ """
+
+# For utils loading.
+from torchtext import data, datasets
+
+def get_data():
+    import spacy
+    spacy_de = spacy.load('de')
+    spacy_en = spacy.load('en')
+
+    def tokenize_de(text):
+        return [tok.text for tok in spacy_de.tokenizer(text)]
+
+    def tokenize_en(text):
+        return [tok.text for tok in spacy_en.tokenizer(text)]
+
+    BOS_WORD = '<s>'
+    EOS_WORD = '</s>'
+    BLANK_WORD = "<blank>"
+    SRC = data.Field(tokenize=tokenize_de, pad_token=BLANK_WORD)
+    TGT = data.Field(tokenize=tokenize_en, init_token = BOS_WORD,
+                     eos_token = EOS_WORD, pad_token=BLANK_WORD)
+
+    MAX_LEN = 100
+    train, val, test = datasets.IWSLT.splits(
+        exts=('.de', '.en'), fields=(SRC, TGT),
+        filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and
+            len(vars(x)['trg']) <= MAX_LEN)
+    MIN_FREQ = 2
+    SRC.build_vocab(train.src, min_freq=MIN_FREQ)
+    TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
+
+    return train, val, test, SRC, TGT
